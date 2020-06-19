2efe735079f897274a3ca4308032e6f118e57319
Dan Husariu <dan.husariu@nttdata.ro>
2020-06-20 00:03:18 +0300

    Rezolvare pentru Tema 3

--- Tema16062020.py
+++ Tema16062020.py
@@ -49,4 +49,24 @@ print(invers(a))
 
 
 #3. According to the creation myths of the Abrahamic religions, Adam and Eve were the first Humans to wander the Earth.
-#You have to do God's job. The creation method must return an array of length 2 containing objects (representing Adam and Eve). The first object in the array should be an instance of the class Man. The second should be an instance of the class Woman. Both objects have to be subclasses of Human. Your job is to implement the Human, Man and Woman classes, then to define the creation function as stated above.
+#You have to do God's job. The creation method must return an array of length 2 containing objects
+# (representing Adam and Eve). The first object in the array should be an instance of the class Man.
+# The second should be an instance of the class Woman. Both objects have to be subclasses of Human.
+# Your job is to implement the Human, Man and Woman classes, then to define the creation function as stated above.
+
+class Om:
+    def __init__(self,nume):
+        self.nume = nume
+
+class Barbat(Om):
+    pass
+class Femeie(Om):
+    pass
+
+def creatie():
+    a = []
+    a.append(Barbat('Adam'))
+    a.append(Femeie('Eva'))
+    return a
+
+print(creatie())
